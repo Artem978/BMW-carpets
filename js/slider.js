b283d5281d1591e9f112b433d75e050b459d9f83
Artem978 <78230712+Artem978@users.noreply.github.com>
2021-12-27 16:45:34 +0400

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,75 @@
+const 
+    leftPrev=document.getElementById('left-arrow-left'),
+    leftNext=document.getElementById('left-arrow-right'),
+    rightPrev=document.getElementById('right-arrow-left'),
+    rightNext=document.getElementById('right-arrow-right'),
+    leftSlides=document.querySelectorAll('.left-slider-image'),
+    rightSlides=document.querySelectorAll('.right-slider-image');
+let 
+    leftIndex=0;
+    rightIndex=0;
+const leftActiveSlide = n =>{
+    for(slide of leftSlides){
+        slide.classList.remove('active');
+    }
+    leftSlides[n].classList.add('active');
+}
+const rightActiveSlide = n =>{
+    for(slide of rightSlides){
+        slide.classList.remove('active');
+    }
+    rightSlides[n].classList.add('active');
+}
+const leftNextSlide = () =>{
+    if (leftIndex==leftSlides.length-1){
+        leftIndex=0;
+        leftActiveSlide(leftIndex);
+    } else{
+        leftIndex++;
+        leftActiveSlide(leftIndex);
+    }
+}
+const leftPrevSlide = () =>{
+    if (leftIndex==0){
+        leftIndex=leftSlides.length-1;
+        leftActiveSlide(leftIndex);
+    } else{
+        leftIndex--;
+        leftActiveSlide(leftIndex);
+    }
+}
+const rightNextSlide = () =>{
+    if (rightIndex==rightSlides.length-1){
+        rightIndex=0;
+        rightActiveSlide(rightIndex);
+    } else{
+        rightIndex++;
+        rightActiveSlide(rightIndex);
+    }
+}
+const rightPrevSlide = () =>{
+    if (rightIndex==0){
+        rightIndex=rightSlides.length-1;
+        rightActiveSlide(rightIndex);
+    } else{
+        rightIndex--;
+        rightActiveSlide(rightIndex);
+    }
+}
+var timer = 0;
+ makeTimer(); 
+ function makeTimer(){
+    clearInterval(timer)
+    timer = setInterval(function(){
+      leftIndex++;
+      leftActiveSlide(leftIndex);
+    },3000);
+    timer = setInterval(function(){
+        rightIndex++;
+        rightActiveSlide(rightIndex);
+      },4000);
+  }
+leftNext.addEventListener('click',leftNextSlide);
+leftPrev.addEventListener('click',leftPrevSlide);
+rightNext.addEventListener('click',rightNextSlide);
+rightPrev.addEventListener('click',rightPrevSlide);
